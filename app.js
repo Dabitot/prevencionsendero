cfd0b7678513e1115d333550b6f64bf08f94c9e8
Silva V. David <dsilvav@sendero.cl>
2023-04-04 13:48:51 -0400

    agregado index

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ res.status(200).send('<h2>hola mundo</h2>')
 
 const PORT = process.env.PORT
 
-app.listen(4000, () =>{
+app.listen(PORT, () =>{
 
 console.log(`servidor escuchando en puerto ${PORT}`)
 
